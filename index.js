2be597a65291e51e3aa37e138396a3cb03e7fcec
Steve Warner <warner@kairosaerospace.com>
2025-04-27 16:48:45 -0700

    Create default variables

--- index.js
+++ index.js
@@ -2,56 +2,61 @@
 
 const PERCENT_OF_SYMPTOMS = 0.7;  // show only 70% of the total symptoms
 
-document.addEventListener('DOMContentLoaded', () => { // ensures page is loaded before this runs
-  // fill placeholders with randomly generated values
-  const age = Math.floor(Math.random() * (80 - 18 + 1) + 18); // random value from 18-80, not scientific!
-  const sex = Math.random() < 0.5 ? 'male' : 'female';
-  const names = ["Alex", "Andy", "Avery", "Blake", "Casey", "Charlie", "Dakota", 
+const POTENTIAL_NAMES = ["Alex", "Andy", "Avery", "Blake", "Casey", "Charlie", "Dakota", 
                  "Devin", "Drew", "Elliot", "Emery", "Finley", "Frankie", "Harper", 
                  "Hayden", "Jamie", "Jordan", "Jules", "Kai", "Kendall", "Lane", 
                  "Logan", "Micah", "Morgan", "Parker", "Quinn", "Reese", "Riley", 
                  "River", "Robin", "Rowan", "Sage", "Sam", "Skylar", "Taylor", "Tatum", 
                  "Toby", "Tyler", "Wren"];
-  const name = names[Math.floor(Math.random() * names.length)];
 
+document.addEventListener('DOMContentLoaded', () => { // ensures page is loaded before this runs
+  const age = Math.floor(Math.random() * (80 - 18 + 1) + 18); // random value from 18-80, not scientific!
+  const sex = Math.random() < 0.5 ? 'male' : 'female';
+  const name = POTENTIAL_NAMES[Math.floor(Math.random() * POTENTIAL_NAMES.length)];  // pick a name at random
 
-  // generate the patient's vitals
-  let level_of_responsiveness = "A&O x4";
-
-  let heart_rate = Math.floor(Math.random() * (100 - 60 + 1) + 60);  // random value from 60-100, not scientific!
-  let heart_strength = "Strong";
-  let heart_rhythm = "Regular";
-  
-  let respiratory_rate = Math.floor(Math.random() * (20 - 12 + 1) + 12);  // random value from 12-20, not scientific!
-  let respiratory_rhythm = "Regular";
-  let respiratory_effort = "Unlabored";
-
-  let skin_color = "Pink"
-  let skin_temperature = "Warm";
-  let skin_moisture = "Dry";
-
+  // generate the patient's default vitals
+  const default_level_of_responsiveness = "A&O x4";
+  const default_heart_rate = Math.floor(Math.random() * (100 - 60 + 1) + 60);  // random value from 60-100, not scientific!
+  const default_heart_strength = "Strong";
+  const default_heart_rhythm = "Regular";
+  const default_respiratory_rate = Math.floor(Math.random() * (20 - 12 + 1) + 12);  // random value from 12-20, not scientific!
+  const default_respiratory_rhythm = "Regular";
+  const default_respiratory_effort = "Unlabored";
+  const default_skin_color = "Pink";
+  const default_skin_temperature = "Warm";
+  const default_skin_moisture = "Dry";
   /*- Mackowiak, P. A., Wasserman, S. S., & Levine, M. M. (1992). A critical appraisal of 98.6°F, the upper limit of 
       the normal body temperature, and other legacies of Carl Reinhold August Wunderlich. JAMA, 268(12), 1578-1580.
     - Sund-Levander, M., Forsberg, C., & Wahren, L. K. (2002). Normal oral, rectal, tympanic, and axillary body 
       temperature in adult men and women: A systematic literature review. Scandinavian Journal of Caring Sciences, 
       16(2), 122-128. */
-  const S = sex == "male" ? 0 : 1
+  const S = sex === "male" ? 0 : 1;
   const eps = Math.random() * 0.5;  // randomness
-  let body_temperature = Math.round(98.2 - (0.02 * age) + (0.3 * S) + eps);
-  let pupils = "Equal, round, and react to light"
-  let blood_pressure = "Strong radial pulse"
+  const default_body_temperature = Math.round(98.2 - (0.02 * age) + (0.3 * S) + eps);
+  const default_pupils = "Equal, round, and react to light";
+  const default_blood_pressure = "Strong radial pulse";
 
-            // <li>Pupils: {{ patient.pupils }}</li>
-            // <li>BP: {{ patient.blood_pressure }}</li>
-
-
+  // these are the actually-used values (which may be modified from default)
+  let level_of_responsiveness = default_level_of_responsiveness;
+  let heart_rate = default_heart_rate;
+  let heart_strength = default_heart_strength;
+  let heart_rhythm = default_heart_rhythm;
+  let respiratory_rate = default_respiratory_rate;
+  let respiratory_rhythm = default_respiratory_rhythm;
+  let respiratory_effort = default_respiratory_effort;
+  let skin_color = default_skin_color;
+  let skin_temperature = default_skin_temperature;
+  let skin_moisture = default_skin_moisture;
+  let body_temperature = default_body_temperature;
+  let pupils = default_pupils;
+  let blood_pressure = default_blood_pressure;
 
   // pick a random condition
   // TODO: pick a condition filtering for the sex
   const condition = conditions[Math.floor(Math.random() * conditions.length)];
   console.log(condition);
   // pick a subset of the symptoms
-  // exclude symptoms where the vital has already been affected
+  // exclude symptoms where the vital has already been affected (i.e. the vital is already different than its default)
 
   // modify the vitals based on the symptoms
 
