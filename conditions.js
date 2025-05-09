daea28723a3ca5cd6636be67f505719b726e798f
Steve Warner <warner@kairosaerospace.com>
2025-05-08 22:03:39 -0700

    Add hypothermia

--- conditions.js
+++ conditions.js
@@ -3,6 +3,9 @@ const symptoms = {
   "Anxiety": {},
   "Can only speak in one or two word clusters": {},
   "Chest tightness": {},
+  "Clumsiness, slow movements and reactions": {},
+  "Cold body temperature (between 95°F and 89.6°F)": {"bodyTemperature": 0.95},
+  "Confusion and poor judgment/loss of awareness": {},
   "Coughing": {},
   "Cyanosis": {"skinColor": "blue"},
   "Difficulty breathing": {"respiratoryEffort": "labored"},
@@ -10,6 +13,8 @@ const symptoms = {
   "Discomfort in the neck": {},
   "Dizziness": {},
   "Dry mouth": {},
+  "Excessive urination": {},
+  "Exhaustion": {},
   "Faintness": {},
   "Fever": {},
   "Hands curl inward and become immobile": {}, // carpopedal spasms
@@ -23,12 +28,13 @@ const symptoms = {
   "Numbness of the hands": {},
   "Numbness of the mouth": {},
   "Pain with inspiration": {},
+  "Pale skin color": {"skinColor": "pale"},
   "Pale, cool, clammy skin": {"skinColor": "pale", "skinTemperature": "cool", "skinMoisture": "clammy"},
   "Rapid and deep breathing": {"respiratoryRate": 1.5, "respiratoryEffort": "deep"},
   "Rapid breathing": {"respiratoryRate": 1.5},
-  "Rapid pulse": {"heartRate": 1.5},
   "Restlessness": {},
   "Shallow breathing": {"respiratoryEffort": "shallow"},
+  "Shivering and chattering teeth": {},
   "Shortness of breath": {"respiratoryRate": 1.5, "respiratoryRhythm": "irregular", "respiratoryEffort": "labored"}, // dyspnea
   "Sleepiness": {},
   "Slow pulse": {"heartRate": .5},
@@ -38,12 +44,50 @@ const symptoms = {
   "Thirst": {},
   "Tingling of the hands": {},
   "Tingling of the mouth": {},
+  "Trouble speaking": {},
   "Vomiting": {},  
   "Weak pulse": {"heartStrength": "weak", "bloodPressure": "weak radial pulse"},
   "Wheezing": {},
 };
 
 const conditions = [
+{
+    name: "Hypothermia (mild)",
+    description: "A cooling of the body's core to a temperature where brain and or muscle function is impaired.",
+    symptoms: [
+    "Cold body temperature (between 95°F and 89.6°F)",
+    "Shivering and chattering teeth",
+    "Exhaustion",
+    "Clumsiness, slow movements and reactions",
+    "Sleepiness",
+    "Weak pulse",
+    "Fast heart rate (tachycardia)",
+    "Rapid breathing (tachypnea)",
+    "Pale skin color",
+    "Confusion and poor judgment/loss of awareness",
+    "Excessive urination",
+    "Trouble speaking",
+    ],
+    treatments: [
+      "Find shelter",
+      "Replace wet with dry, insulating clothing",
+      "Wind and waterproof layers",
+      "Exercise is appropriate for mildly hypothermic patients",
+      "Insulate the head and neck, hands and feet",
+      "Consider a hypothermia wrap",
+      "Add head packs/hot water bottles, insulate to prevent burns",
+      "Encourage the patient to eat a meal",
+      "Give warm, sweet, non-caffeinated, non-alcoholic liquids if the patient can swallow",
+
+    ],
+    evacuationGuidelines: [
+      "Evacuate rapidly any patient who does not respond to warming efforts",
+    ],
+    references: [
+      "https://my.clevelandclinic.org/health/diseases/21164-hypothermia-low-body-temperature",
+      "NOLS Wilderness Medicine Field Guide, 7th edition"
+    ]
+  },
   {
     name: "Angina",
     description: "Pain from diminished blood flow to the heart.",
